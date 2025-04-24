40532bca9e03118e756bb2ac4f4e219c01061521
larissaorcx <larissaorcx@gmail.com>
2025-04-23 22:44:44 -0300

    🚧 feat: funções para inscricao e pagamento no stripe

---
+++ app/server/stripe/handle-payment.ts
@@ -0,0 +1,26 @@
+import { db } from "@/app/lib/firebase"
+import "server-only"
+
+import Stripe from "stripe"
+
+export async function handleStripePayment(event: Stripe.CheckoutSessionCompletedEvent){
+    if(event.data.object.payment_status ==="paid"){
+        console.log("Pagamento realizado com sucesso. Enviar um email e liberar acesso")
+
+        const metadata = event.data.object.metadata 
+    
+        const userId = metadata?.userId
+    
+        if(!userId){
+            console.log("User ID not found")
+            return ;
+        }
+    
+        await db.collection("users").doc(userId).update({
+            stripeSubscription: event.data.object.subscription,
+            subscriptionStatus: "active"
+        })
+    }
+}
+
+
