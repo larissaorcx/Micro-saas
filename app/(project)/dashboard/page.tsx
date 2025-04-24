87e7c83479b6fce37adfb3657c2bc293ccccc5fb
larissaorcx <larissaorcx@gmail.com>
2025-04-23 22:38:55 -0300

    🚧 feat: adicionando o link para a página de pagamentos

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,27 +1,31 @@
 import { handleAuth } from "@/app/actions/handle-auth";
 import { auth } from "@/app/lib/auth";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
-    //estamos no lado do servidor
-    const session = await auth()
-
-    if(!session){
-        redirect("/login")
-        console.log(session)
-    }
-
+  //estamos no lado do servidor
+  const session = await auth()
+  
+  if(!session){
+    redirect("/login")
+    console.log(session)
+  }
+  
   return (
     <div className="flex flex-col gap-10 items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Protected Dashboard</h1>
-      <p>{session?.user?.email ? session?.user?.email : "Usuário não está logado"}</p>
-      {
-        session.user?.email && "larissaorcx@gmail.com" &&(
-            <form action={handleAuth}>
-                <button type="submit" className="rounded-md border px-2 cursor-pointer ">Logout</button>
-            </form>
-        )
-      }
+    <h1 className="text-4xl font-bold">Protected Dashboard</h1>
+    <p>{session?.user?.email ? session?.user?.email : "Usuário não está logado"}</p>
+    {
+      session.user?.email && "larissaorcx@gmail.com" &&(
+        <form action={handleAuth}>
+        <button type="submit" className="rounded-md border px-2 cursor-pointer ">Logout</button>
+        </form>
+      )
+    }
+    <Link href="/pagamentos" className="rounded-md border px-2 cursor-pointer ">
+      Pagamentos
+    </Link>
     </div>
   );
 }
