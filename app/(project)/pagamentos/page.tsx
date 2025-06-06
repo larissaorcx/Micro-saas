84b45a8ae343c8b84b4e701049c3c5e175f3030e
larissaorcx <larissaorcx@gmail.com>
2025-06-05 23:25:16 -0300

    📝 feat: novo botão para o checkout do mercado pago

--- app/(project)/pagamentos/page.tsx
+++ app/(project)/pagamentos/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { useStripe } from "@/app/hooks/useStripe";
+import useMercadoPago from "@/app/hooks/usMercadoPago";
 
 export default function Pagamentos() {
     const { createPaymetStipeCheckout, createSubscriptionCheckout, handleCreateStripePortal} = useStripe()
+
+    const { createMercadoPagoCheckout } = useMercadoPago();
     return(
         <div className="flex flex-col gap-10 items-center justify-center h-screen">
             <h1 className="text-4xl font-bold">Pagementos</h1>
@@ -17,6 +20,13 @@ export default function Pagamentos() {
             </button>
             <button type="button" className="border rounded-md px-1 cursor-pointer" onClick={() => handleCreateStripePortal()}>
                 Criar Portal de Pagamentos
+            </button>
+            <button type="button" className="border rounded-md px-1 cursor-pointer" onClick={() => createMercadoPagoCheckout(
+                {   testeId: "123", 
+                    userEmail: "teste@teste.com"
+                }
+            )}>
+                Criar Pagamento com Mercado Pago
             </button>
         </div>
     )
